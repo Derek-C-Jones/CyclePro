e8c9d9625271d5cd3d65df3821c12cf6379a639a
Derek-C-Jones <derek.christopher.jones@gmail.com>
2015-12-09 19:32:45 +0200

    Renamed data folder

---
+++ Models/Extensions/Route.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclePro.Models
+{
+    public partial class Route
+    {
+        public List<Point> RemovePoint(string lat, string lon)
+        {
+            this.Points.RemoveAll(p => p.Latitude == lat && p.Longitude == lon);
+            return this.Points;
+        }
+    }
+}
